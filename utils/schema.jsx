648a2925e33e6e8e38f92db3a59f95b0beb5b7c6
ayush-patel1 <ayush005rkt@gmail.com>
2025-01-24 16:40:08 +0530

    setup database connection

--- utils/schema.jsx
+++ utils/schema.jsx
@@ -3,3 +3,11 @@ import {
 } from 'drizzle-orm/pg-core'
 
 //budget
+export const Budgets = pqTable('budget',{
+   id:serial('id').primaryKey(),
+   name:varchar('name').notNull(),
+    amount:varchar('amount').notNull(),
+    Icon:varchar('Icon'),
+    createdBy:varchar('createdBy').notNull(),
+})
+
